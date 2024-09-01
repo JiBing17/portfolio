6223414b98668e5ad1bcfd2833e600adb046f56f
Ji Bing Ni <jibingni17@gmail.com>
2024-08-31 21:10:51 -0400

    added hover effect of color change and size change for all icons and page links on navbar

--- src/App.js
+++ src/App.js
@@ -39,22 +39,48 @@ function App() {
         {/* PFP pic, name, anchor icons*/}
         <Box component="section" sx={{ 
           display: "flex", flexDirection: "column", justifyContent: "space-around",
-          alignItems: "center", p: 2, border: '1px dashed grey' 
+          alignItems: "center", p: 4, gap: 1.5 
           }}>
+
           <img src={profilePic} alt="Profile Pic" className='rounded-full size-32 border-4 border-white'/>
-          <h1 className='text-white text-2xl'>Ji Bing Ni</h1>
-            <Box sx={{ display: 'flex', justifyItems: 'space-around', p: 4 }}>
+          <Typography sx={{ color: "white", fontSize: "1.5rem"}}> Ji Bing Ni</Typography>
+
+            <Box sx={{ display: 'flex', justifyContent: 'space-around', gap: 1.5}}>
               <a href='https://leetcode.com/u/Ji_Bing/' target="_blank" rel="noreferrer">
-                <CodeIcon sx={{ color: "white"}}/>
+                <CodeIcon sx={{ color: "white" , backgroundColor: "#A8A9B4", borderRadius: "50%", padding: 1, fontSize: "2rem", 
+                // hover effect for background color change and size increase
+                "&:hover":{
+                  backgroundColor: "#ff00ff", // Change background color on hover
+                  transform: "scale(1.1)"
+                }  }}/>
               </a>
               <a href='https://github.com/JiBing17' target="_blank" rel="noreferrer">
-                <GitHubIcon sx={{ color: "white"}} />
+                <GitHubIcon sx={{ color: "white", backgroundColor: "#A8A9B4", borderRadius: "50%", padding: 1, fontSize: "2rem",
+                // hover effect for background color change and size increase
+                "&:hover":{
+                  backgroundColor: "#ff00ff", // Change background color on hover
+                  transform: "scale(1.1)"
+                }
+                }} />
               </a>
-              <a href='https://www.linkedin.com/in/jibing-ni/' target="_blank" rel="noreferrer">
-                <LinkedInIcon sx={{ color: "white"}} />
+
+              <a href='https://github.com/JiBing17' target="_blank" rel="noreferrer">
+                <LinkedInIcon sx={{ color: "white", backgroundColor: "#A8A9B4", borderRadius: "50%", padding: 1, fontSize: "2rem",
+                // hover effect for background color change and size increase
+                "&:hover":{
+                  backgroundColor: "#ff00ff", // Change background color on hover
+                  transform: "scale(1.1)"
+                }
+                }} />
               </a>
+
               <a href='https://www.linkedin.com/in/jibing-ni/' target="_blank" rel="noreferrer">
-                <DescriptionIcon sx={{ color: "white"}} />
+                <DescriptionIcon sx={{ color: "white", backgroundColor: "#A8A9B4", borderRadius: "50%", padding: 1, fontSize: "2rem",
+                // hover effect for background color change and size increase
+                "&:hover":{
+                  backgroundColor: "#ff00ff", 
+                  transform: "scale(1.1)"
+                }}} />
               </a>
             </Box>
         </Box>
@@ -63,46 +89,211 @@ function App() {
         <Box sx={{display:"flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around", height: "100%"}}>
 
           {/* Home*/}
-          <a href='./' target="_blank" rel="noreferrer">
-            <Box sx={{display: "flex", alignItems: "center"}}>
-              <HomeIcon sx={{ color: "white"}} />
-              <Typography sx={{ color: "#A8A9B4"}}>Home</Typography>
+          <a href='./' target="_blank" rel="noreferrer" style={{ width: '100%' }}>
+            <Box 
+              sx={{
+                display: "flex", 
+                alignItems: "center",
+                width: "100%",
+                justifyContent: "center", 
+
+                // hover effect used for text color change, icon color change, and size change for both
+                '&:hover': {
+                  '& .home-icon': {
+                    color: "#ff00ff", 
+                    transform: "scale(1.1)", 
+                  },
+                  '& .home-text': {
+                    color: "white", 
+                    transform: "scale(1.1)", 
+                  },
+                },
+              }}
+            >
+              <HomeIcon 
+                className="home-icon"
+                sx={{ 
+                  color: "white", 
+                  marginRight: ".5rem",
+                  transition: "transform 0.3s ease, color 0.3s ease", 
+                }} 
+              />
+              <Typography 
+                className="home-text"
+                sx={{ 
+                  color: "#A8A9B4", 
+                  transition: "transform 0.3s ease, color 0.3s ease",
+                }}
+              >
+                Home
+              </Typography>
+
+              
             </Box>
           </a>
           {/* About */}
-          <a href='https://www.linkedin.com/in/jibing-ni/' target="_blank" rel="noreferrer">
-            <Box sx={{display: "flex", alignItems: "center"}}>
-              <PersonIcon sx={{ color: "white"}} /> 
-              <Typography sx={{ color: "#A8A9B4"}}> About</Typography>
+          <a href='https://www.linkedin.com/in/jibing-ni/' target="_blank" rel="noreferrer" style={{ width: '100%' }}>
+          
+          <Box 
+              sx={{
+                display: "flex", 
+                alignItems: "center",
+                width: "100%",
+                justifyContent: "center", 
+
+                // hover effect used for text color change, icon color change, and size change for both
+                '&:hover': {
+                  '& .about-icon': {
+                    color: "#ff00ff", 
+                    transform: "scale(1.1)", 
+                    transition: "transform 0.3s ease, color 0.3s ease", 
+
+                  },
+                  '& .about-text': {
+                    color: "white", 
+                    transform: "scale(1.1)", 
+                    transition: "transform 0.3s ease, color 0.3s ease",
+
+                  },
+                },
+              }}
+            >
+              <PersonIcon className="about-icon" sx={{ color: "white", marginRight: ".5rem"}} /> 
+              <Typography className="about-text" sx={{ color: "#A8A9B4"}}> About</Typography>
+
             </Box>
+
+
+              
           </a>
           {/* Resume */}
-          <a href='https://www.linkedin.com/in/jibing-ni/' target="_blank" rel="noreferrer">
-            <Box sx={{display: "flex", alignItems: "center"}}>
-              <DescriptionIcon sx={{ color: "white"}} /> 
-              <Typography sx={{ color: "#A8A9B4"}}>Resume</Typography>
+          <a href='https://www.linkedin.com/in/jibing-ni/' target="_blank" rel="noreferrer" style={{ width: '100%' }}>
+
+          <Box 
+              sx={{
+                display: "flex", 
+                alignItems: "center",
+                width: "100%",
+                justifyContent: "center", 
+
+                // hover effect used for text color change, icon color change, and size change for both
+                '&:hover': {
+                  '& .resume-icon': {
+                    color: "#ff00ff", 
+                    transform: "scale(1.1)", 
+                    transition: "transform 0.3s ease, color 0.3s ease",
+
+                  },
+                  '& .resume-text': {
+                    color: "white", 
+                    transform: "scale(1.1)", 
+                    transition: "transform 0.3s ease, color 0.3s ease", 
+
+                  },
+                },
+              }}
+            >
+              <DescriptionIcon className="resume-icon" sx={{ color: "white", marginRight: ".5rem"}} /> 
+              <Typography className="resume-text" sx={{ color: "#A8A9B4",}}>Resume</Typography>
             </Box>
           </a>
+
           {/* Projects */}
-          <a href='https://www.linkedin.com/in/jibing-ni/' target="_blank" rel="noreferrer">
-            <Box sx={{display: "flex", alignItems: "center"}}>
-              <ProjectIcon sx={{ color: "white"}} /> 
-              <Typography sx={{ color: "#A8A9B4"}}>Projects</Typography>
-            </Box>
+          <a href='https://www.linkedin.com/in/jibing-ni/' target="_blank" rel="noreferrer" style={{ width: '100%' }}>
+
+          <Box 
+              sx={{
+                display: "flex", 
+                alignItems: "center",
+                width: "100%",
+                justifyContent: "center", 
+
+                // hover effect used for text color change, icon color change, and size change for both
+                '&:hover': {
+                  '& .projects-icon': {
+                    color: "#ff00ff", 
+                    transform: "scale(1.1)",
+                    transition: "transform 0.3s ease, color 0.3s ease",
+
+                  },
+                  '& .projects-text': {
+                    color: "white", 
+                    transform: "scale(1.1)", 
+                    transition: "transform 0.3s ease, color 0.3s ease", 
+
+                  },
+                },
+              }}
+            >
+              <ProjectIcon className="projects-icon" sx={{ color: "white", marginRight: ".5rem"}} /> 
+              <Typography className="projects-text" sx={{ color: "#A8A9B4"}}>Projects</Typography>
+            </Box>              
           </a>
+
+
+
           {/* Services */}
-          <a href='https://www.linkedin.com/in/jibing-ni/' target="_blank" rel="noreferrer">
-            <Box sx={{display: "flex", alignItems: "center"}}>
-              <SettingsIcon sx={{ color: "white"}} /> 
-              <Typography sx={{ color: "#A8A9B4"}}>Services</Typography>
+          <a href='https://www.linkedin.com/in/jibing-ni/' target="_blank" rel="noreferrer" style={{ width: '100%' }}>
+
+          <Box 
+              sx={{
+                display: "flex", 
+                alignItems: "center",
+                width: "100%",
+                justifyContent: "center", 
+
+                // hover effect used for text color change, icon color change, and size change for both
+                '&:hover': {
+                  '& .services-icon': {
+                    color: "#ff00ff", 
+                    transform: "scale(1.1)", 
+                    transition: "transform 0.3s ease, color 0.3s ease", 
+
+                  },
+                  '& .services-text': {
+                    color: "white", 
+                    transform: "scale(1.1)",
+                    transition: "transform 0.3s ease, color 0.3s ease", 
+
+                  },
+                },
+              }}
+            >
+              <SettingsIcon className="services-icon" sx={{ color: "white", marginRight: ".5rem"}} /> 
+              <Typography className="services-text" sx={{ color: "#A8A9B4"}}>Services</Typography>
             </Box>
           </a>
           {/* Contact */}
-          <a href='https://www.linkedin.com/in/jibing-ni/' target="_blank" rel="noreferrer">
-            <Box sx={{display: "flex", alignItems: "center"}}>
-              <EmailIcon sx={{ color: "white"}} /> 
-              <Typography sx={{ color: "#A8A9B4"}}>Contact</Typography>
+          <a href='https://www.linkedin.com/in/jibing-ni/' target="_blank" rel="noreferrer" style={{ width: '100%' }}>
+
+          <Box 
+              sx={{
+                display: "flex", 
+                alignItems: "center",
+                width: "100%",
+                justifyContent: "center",
+                
+                // hover effect used for text color change, icon color change, and size change for both
+                '&:hover': {
+                  '& .contact-icon': {
+                    color: "#ff00ff", 
+                    transform: "scale(1.1)", 
+                    transition: "transform 0.3s ease, color 0.3s ease", 
+
+                  },
+                  '& .contact-text': {
+                    color: "white", 
+                    transform: "scale(1.1)", 
+                    transition: "transform 0.3s ease, color 0.3s ease",
+
+                  },
+                },
+              }}
+            >
+              <EmailIcon className="contact-icon" sx={{ color: "white", marginRight: ".5rem"}} /> 
+              <Typography className="contact-text" sx={{ color: "#A8A9B4"}}>Contact</Typography>
             </Box>
+              
           </a>
         </Box>
       </Paper>
