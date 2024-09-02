0ac643e70debbb08540761b8024114ce32ecc725
Ji Bing Ni <jibingni17@gmail.com>
2024-09-01 22:04:02 -0400

    added icons for each skill + updated visuals

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPython, faJava, faJs, faReact, faBootstrap } from '@fortawesome/free-brands-svg-icons';
 
+import { SiC, SiCplusplus } from 'react-icons/si'; // Import from the 'si' (Simple Icons) set
 
 function App() {
   return (
@@ -364,52 +365,108 @@ function App() {
           </Box>
 
           <Typography sx={{fontSize: "2rem",  borderBottom: '3px solid #ff00ff', display: "inline", paddingBottom: ".2rem",  fontWeight:"bold"}}> Skills</Typography>
-          <Typography sx={{}}>
+          <Typography sx={{marginTop: "1rem"}}>
             Throughout my career, I have gained significant experience and skills in various areas of this field.
           </Typography>
 
-
-          <Box sx={{ display:"flex", alignItems: "center", justifyContent:"space-around", gap: 2}}>
-            <Box>
-              <Typography sx={{fontSize: "2rem"}}> Languages</Typography>
-              <Typography sx={{}}>Python 
-                <FontAwesomeIcon icon={faPython} size="lg" style={{ marginRight: '8px' }} />
-              </Typography> 
-
-              <Typography sx={{}}>C</Typography>
-              <Typography sx={{}}>C++</Typography>
-              <Typography sx={{}}>Java 
-                <FontAwesomeIcon icon={faJava} size="lg" style={{ marginRight: '8px' }} />
-              </Typography>
-              <Typography sx={{}}>Javascript
-                <FontAwesomeIcon icon={faJs} size="lg" style={{ marginRight: '8px' }} />
-              </Typography>
-            </Box>
+          <Box sx={{ display: "flex", alignItems: "flex-start", justifyContent: "space-around", gap: 4, padding: "1rem"}}>
             
-            <Box>
-              <Typography sx={{fontSize: "2rem"}}> Frameworks</Typography>
-              <Typography sx={{}}>React
-                <FontAwesomeIcon icon={faReact} size="lg" style={{ marginRight: '8px' }} />
-              </Typography>
-              <Typography sx={{}}>Flask</Typography>
-            </Box>
-            
-            <Box>
-              <Typography sx={{fontSize: "2rem"}}> Databases</Typography>
-              <Typography sx={{}}>MongoDB</Typography>
-            </Box>
-            
-            <Box>
-              <Typography sx={{fontSize: "2rem"}}> Libraries</Typography>
-              <Typography sx={{}}>Material UI</Typography>
-              <Typography sx={{}}>Bootstrap
-                <FontAwesomeIcon icon={faBootstrap} size="lg" style={{ marginRight: '8px' }} />
+            <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around"}}>
+              <Typography sx={{ fontSize: "2rem", marginBottom: 1 }}>Languages</Typography>
 
+              <Typography sx={{ display: "inline-flex", alignItems: "center", marginBottom: 1 }}>
+                Python 
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" 
+                    alt="Python" 
+                    style={{ width: "24px", height: "24px", marginLeft: "0.5rem" }} />
               </Typography>
-              <Typography sx={{}}>Software and Systems Engineer</Typography>
+
+              <Typography sx={{ display: "inline-flex", alignItems: "center", marginBottom: 1 }}>
+                C
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" 
+                    alt="C" 
+                    style={{ width: "24px", height: "24px", marginLeft: "0.5rem" }} />
+              </Typography>
+
+              <Typography sx={{ display: "inline-flex", alignItems: "center", marginBottom: 1 }}>
+                C++
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" 
+                    alt="C++" 
+                    style={{ width: "24px", height: "24px", marginLeft: "0.5rem" }} />
+              </Typography>
+
+              <Typography sx={{ display: "inline-flex", alignItems: "center", marginBottom: 1 }}>
+                Java 
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg" 
+                    alt="Java" 
+                    style={{ width: "24px", height: "24px", marginLeft: "0.5rem" }} />
+              </Typography>
+
+              <Typography sx={{ display: "inline-flex", alignItems: "center", marginBottom: 1 }}>
+                Javascript
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" 
+                    alt="Javascript" 
+                    style={{ width: "24px", height: "24px", marginLeft: "0.5rem" }} />
+              </Typography>
             </Box>
 
-          </Box>
+            <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around"}}>
+              <Typography sx={{ fontSize: "2rem", marginBottom: 1 }}>Frameworks</Typography>
+
+              <Typography sx={{ display: "inline-flex", alignItems: "center", marginBottom: 1 }}>
+                React
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" 
+                    alt="React" 
+                    style={{ width: "24px", height: "24px", marginLeft: "0.5rem" }} />
+              </Typography>
+
+              <Typography sx={{ display: "inline-flex", alignItems: "center", marginBottom: 1 }}>
+                Flask
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg" 
+                    alt="Flask" 
+                    style={{ width: "24px", height: "24px", marginLeft: "0.5rem" }} />
+              </Typography>
+            </Box>
+
+            <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around"}}>
+              <Typography sx={{ fontSize: "2rem", marginBottom: 1 }}>Databases</Typography>
+
+              <Typography sx={{ display: "inline-flex", alignItems: "center", marginBottom: 1 }}>
+                MongoDB
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg" 
+                    alt="MongoDB" 
+                    style={{ width: "24px", height: "24px", marginLeft: "0.5rem" }} />
+              </Typography>
+            </Box>
+
+            <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "space-around"}}>
+              <Typography sx={{ fontSize: "2rem", marginBottom: 1 }}>Libraries</Typography>
+
+              <Typography sx={{ display: "inline-flex", alignItems: "center", marginBottom: 1 }}>
+                Material UI
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/materialui/materialui-original.svg" 
+                    alt="Material UI" 
+                    style={{ width: "24px", height: "24px", marginLeft: "0.5rem" }} />
+              </Typography>
+
+              <Typography sx={{ display: "inline-flex", alignItems: "center", marginBottom: 1 }}>
+                Tailwind CSS 
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
+                    alt="Tailwind CSS" 
+                    style={{ width: "24px", height: "24px", marginLeft: "0.5rem" }} />
+              </Typography>
+
+              <Typography sx={{ display: "inline-flex", alignItems: "center", marginBottom: 1 }}>
+                Bootstrap
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" 
+                    alt="Bootstrap" 
+                    style={{ width: "24px", height: "24px", marginLeft: "0.5rem" }} />
+              </Typography>
+
+
+            </Box>
+</Box>
+
           
           
 
