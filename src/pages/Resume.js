fbfe6f8c30486b858eeceed95182c85aeebd991d
Ji Bing Ni <jibingni17@gmail.com>
2024-09-02 21:56:17 -0400

    added education on resume section

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -6,6 +6,8 @@ const Resume = () => {
     <>
         <Box sx={{padding: "1rem"}}>
             <Typography sx={{fontSize: "2rem",  borderBottom: '3px solid #ff00ff', display: "inline", paddingBottom: ".2rem",  fontWeight:"bold"}}> Resume</Typography>
+
+            {/* Summary */}
             <Box display="flex" mt={10}>
                 {/* Left Column for Circle and Line Visual */}
                 <Box display="flex" flexDirection="column" alignItems="center" position="relative">
@@ -29,7 +31,7 @@ const Resume = () => {
                 <Box ml={6}>
                     <Typography sx={{ fontSize: "1.5rem", fontWeight: "bold" }}> Summary </Typography>
                     <Box mt={2}>
-                    <Typography variant="h4" component="h4" sx={{ fontSize: "1.25rem", fontWeight: "bold" }}>
+                    <Typography variant="h4" component="h4" sx={{ fontSize: "1.25rem", fontWeight: "bold", color: "#606060" }}>
                         Ji Bing Ni
                     </Typography>
                     <Typography variant="body1" sx={{ fontStyle: "italic", mt: 1 }}>
@@ -42,7 +44,45 @@ const Resume = () => {
                     </Box>
                     </Box>
                 </Box>
+            </Box>
+
+            {/* Education */}
+            <Box display="flex" mt={10}>
+                {/* Left Column for Circle and Line Visual */}
+                <Box display="flex" flexDirection="column" alignItems="center" position="relative">
+                    {/* Unfilled Circle */}
+                    <Box 
+                    width="16px" 
+                    height="16px" 
+                    border="2px solid #ff00ff" 
+                    borderRadius="50%" 
+                    zIndex={10} 
+                    bgcolor="white" 
+                    />
+                    {/* Line */}
+                    <Box 
+                    width="2px" 
+                    bgcolor="#ff00ff" 
+                    flexGrow={1} 
+                    />
                 </Box>
+                {/* Right Column for Content */}
+                <Box ml={6}>
+                    <Typography sx={{ fontSize: "1.5rem", fontWeight: "bold" }}> Education </Typography>
+                    <Box mt={2}>
+                    <Typography variant="h4" component="h4" sx={{ fontSize: "1.25rem", fontWeight: "bold", color: "#606060" }}>
+                        Bachelor of Science in Computer Science
+                    </Typography>
+                    <Typography variant="body1" sx={{ fontStyle: "italic", fontWeight: "bold", mt: 1 }}>
+                        2022- 2026 (Expected)
+                    </Typography>
+                    <Typography variant="body1" sx={{ fontStyle: "italic", mt: 1 }}>
+                        Purdue University, West Lafayette, IN
+                    </Typography>
+                    </Box>
+                </Box>
+            </Box>
+            
         </Box>
 
 
