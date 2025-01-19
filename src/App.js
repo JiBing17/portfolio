846d50434bfa01de65614588b33c4cd00af9f93e
Ji Bing Ni <jibingni17@gmail.com>
2025-01-19 16:13:51 -0500

    redid hero section

--- src/App.js
+++ src/App.js
@@ -10,19 +10,7 @@ import Resume from './pages/Resume'
 function App() {
   return (
     <>
-      <Navbar/>
-
-      {/* Main Content Area */}
-      <Box
-        sx={{
-          flexGrow: 1,
-          marginLeft: '18%', 
-        }}
-      >
-        <Hero/>
-        <About/>
-        <Resume/>
-      </Box>
+      <Hero/>
     </>
   );
 }
--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,34 +1,93 @@
 import React from 'react';
-import bgPic from '../icons/bg.webp'
-import { Box, Typography, Button, } from '@mui/material';
+import { Box, Typography, Link, Stack } from '@mui/material';
+import bgPic from '../icons/hero_background.png';
+import HomeIcon from '@mui/icons-material/Home';
+import PersonIcon from '@mui/icons-material/Person';
+import EditIcon from '@mui/icons-material/Edit';
+import WorkIcon from '@mui/icons-material/Work';
+import DescriptionIcon from '@mui/icons-material/Description';
+import { TypeAnimation } from 'react-type-animation';
 
 const Hero = () => {
   return (
     <>
-    {/* Intro Section */}
+      {/* Hero Background Image */}
+      <Box
+        sx={{
+          position: 'relative',
+          height: '100vh',
+          backgroundImage: `url(${bgPic})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        {/* Container for nav links and hero text */}
         <Box
-            sx={{
-            position: "relative",
-            height: '100vh',
-            backgroundImage: `url(${bgPic})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            
-            }}
+          sx={{
+            display: 'flex',
+            position: 'absolute',
+            top: '30%',
+            left: '2%',
+            padding: '1rem',
+            alignItems: 'center',
+            justifyContent: 'start',
+            width: '100%',
+          }}
         >
-            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "space-around", justifyContent: "center",position: 'absolute',
-            top: '30%',left: '0', padding: "1rem"}}>
-                <Typography sx={{ color: "white", fontSize: "5rem", fontWeight: "bold"}}>
-                    Ji Bing Ni
-                </Typography>
-                <Typography sx={{ color: "white", fontSize: "2rem"}}>
-                    I am a Learner
-                </Typography>
-                <Button variant="outlined" sx={{ color: "white", fontSize: "2rem", color: "white", borderColor: "white"}}>View My Projects</Button>
-            </Box> 
+          {/* Navigation Links */}
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginRight: '9rem',
+              gap: '.5rem',
+            }}
+          >
+            <Link href="/home" underline="hover" sx={{ backgroundColor: 'white', borderRadius: '50%', padding: '.5rem' }}>
+              <HomeIcon sx={{ fontSize: '2rem', color: 'black' }} />
+            </Link>
+            <Link href="/about" underline="hover" sx={{ backgroundColor: 'white', borderRadius: '50%', padding: '.5rem' }}>
+              <PersonIcon sx={{ fontSize: '2rem', color: 'black' }} />
+            </Link>
+            <Link href="/skills" underline="hover" sx={{ backgroundColor: 'white', borderRadius: '50%', padding: '.5rem' }}>
+              <EditIcon sx={{ fontSize: '2rem', color: 'black' }} />
+            </Link>
+            <Link href="/projects" underline="hover" sx={{ backgroundColor: 'white', borderRadius: '50%', padding: '.5rem' }}>
+              <WorkIcon sx={{ fontSize: '2rem', color: 'black' }} />
+            </Link>
+            <Link href="/resume" underline="hover" sx={{ backgroundColor: 'white', borderRadius: '50%', padding: '.5rem' }}>
+              <DescriptionIcon sx={{ fontSize: '2rem', color: 'black' }} />
+            </Link>
+          </Box>
+
+          {/* Hero Text */}
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'start', justifyContent: 'center' }}>
+            <Typography sx={{ color: 'white', fontSize: '5rem', fontWeight: 'bold' }}>Ji Bing Ni</Typography>
+            <Typography sx={{ color: 'white', fontSize: '2rem', marginTop: '-1rem' }}>
+              I'm a {' '}
+              <TypeAnimation
+                sequence={['CS major @ Purdue ', 1000, 'Learner', 1000, 'Developer', 1000, 'Designer', 1000]}
+                speed={{ type: 'keyStrokeDelayInMs', value: 100 }} // Adjust the typing speed here
+                style={{ display: 'inline-block', color: "#f7cac9"}}
+                repeat={Infinity}
+              />
+            </Typography>
+            <Stack direction="row" spacing={2}>
+              <Link href="https://github.com/JiBing17" underline="hover">
+                <i className="fab fa-github text-white text-2xl"></i>
+              </Link>
+              <Link href="https://www.linkedin.com/in/jibing-ni/" underline="hover">
+                <i className="fab fa-linkedin text-white text-2xl"></i>
+              </Link>
+            </Stack>
+          </Box>
         </Box>
+      </Box>
     </>
   );
 };
 
-export default Hero
+export default Hero;
+
